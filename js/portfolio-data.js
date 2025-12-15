1e9bd521ec9a488214ba997f22a7668645ba746d
MohammadHassan <mhze.uk@gmail.com>
2025-12-15 12:47:25 +0000

    feat: removed the admin stuff

--- js/portfolio-data.js
+++ js/portfolio-data.js
@@ -2,24 +2,24 @@
 // Portfolio Data
 // Add your portfolio items below as objects in the array.
 // Example:
-{
-  title: "Gas Mask Soldier",
-  description; " A stylized gas mask soldier character created for a post-apocalyptic game.",
-  software; "Blender, ZBrush, Substance Painter",
-  category; "Stylized Character",
-  date; "2025-12-15",
-  polygons; "15,000",
-  tags; ["stylized", "game art"],
-  images; {
-    final; "\\images\\portfolio\\gas-mask-soldier-character.webp",
-    basecolor; "images/character1-basecolor.jpg",
-    normal; "images/character1-normal.jpg",
-    wireframe; "images/character1-wireframe.jpg",
-    nocolor; "images/character1-nocolor.jpg"
-  }
-}
 
 const portfolioData = [
+  {
+    title: "Gas Mask Soldier",
+    description: "A stylized gas mask soldier character created for a post-apocalyptic game.",
+    software: "Blender, ZBrush, Substance Painter",
+    category: "Stylized Character",
+    date: "2025-12-15",
+    polygons: "15,000",
+    tags: ["stylized", "game art"],
+    images: {
+      final: "images/portfolio/gas-mask-soldier-character.webp",
+      basecolor: "images/character1-basecolor.jpg",
+      normal: "images/character1-normal.jpg",
+      wireframe: "images/character1-wireframe.jpg",
+      nocolor: "images/character1-nocolor.jpg"
+    }
+  },
     // Example item:
     // {
     //   title: "Elf Warrior",
